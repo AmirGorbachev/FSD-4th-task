aadcd205b25ea740f0c12dad0ef16a423964e3d9
Amir Gorebachev <32037802+AmirGorbachev@users.noreply.github.com>
2020-09-18 15:32:44 +0700

    Работа над range-slider

--- src/plugin/green-range-slider.js
+++ src/plugin/green-range-slider.js
@@ -1,6 +1,8 @@
 // Стили плагина
 import "./green-range-slider.sass";
 
+let rangeSlider = new RangeSlider();
+
 document.addEventListener('DOMContentLoaded', () => {
   createSliderElements("green-range-slider-here");
 });
@@ -24,5 +26,71 @@ function createSliderElements(nameContainer) {
                        rangeSliderButtonMax);
 
     container.append(rangeSlider);
+
+    /**/
+
+    getCoords(rangeSlider);
   }
+}
+
+/**/
+
+class RangeSlider {
+  constructor() {
+
+  }
+
+  getCoords() {
+    let coords = elem.getBoundingClientRect();
+    let RangeSlider = {};
+
+    RangeSlider.top = coords.top +
+               window.pageYOffset;
+    RangeSlider.left = coords.left +
+                window.pageXOffset;
+    RangeSlider.rigth = coords.left +
+                 window.pageXOffset +
+                 coords.width;
+    RangeSlider.bottom = coords.top +
+                  window.pageYOffset +
+                  coords.height
+    RangeSlider.width = coords.width;
+
+    console.log(RangeSlider)
+    console.log("next")
+  }
+}
+
+let rangeSlider = new RangeSlider();
+
+function getCoords(elem) {
+  let coords = elem.getBoundingClientRect();
+  let RangeSlider = {};
+
+  RangeSlider.top = coords.top +
+             window.pageYOffset;
+  RangeSlider.left = coords.left +
+              window.pageXOffset;
+  RangeSlider.rigth = coords.left +
+               window.pageXOffset +
+               coords.width;
+  RangeSlider.bottom = coords.top +
+                window.pageYOffset +
+                coords.height
+  RangeSlider.width = coords.width;
+
+  console.log(RangeSlider)
+  console.log("next")
+}
+
+function moveRange() {
+
+}
+
+function onMouseMove() {
+
+}
+
+function onMouseUp() {
+
 }
