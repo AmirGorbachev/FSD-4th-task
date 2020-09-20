1e21fcf0768ac44d7fa00930233e094f91097e54
Amir Gorebachev <32037802+AmirGorbachev@users.noreply.github.com>
2020-09-20 12:21:09 +0700

    Добавил шаблон плагинов на jQuery

--- src/plugin/green-range-slider.js
+++ src/plugin/green-range-slider.js
@@ -1,87 +1,29 @@
 // Стили плагина
 import "./green-range-slider.sass";
 
-class RangeSlider {
-  constructor() {}
-  getCoords(element) {
-    let coords = element.getBoundingClientRect();
-    let RangeSlider = {};
+;(function ($, window, document, undefined) {
+  let pluginName = "green-range-slider",
+      defaults = {
+        propertyName = "value",
+      };
+  function Plugin(element, options) {
+    this.element = element;
+    this.options = $.extend({}, defaults, options);
 
-    this.top = coords.top + window.pageYOffset;
-    this.left = coords.left + window.pageXOffset;
-    this.rigth = coords.left + window.pageXOffset +
-                 coords.width;
-    this.bottom = coords.top + window.pageYOffset +
-                  coords.height
-    this.width = coords.width;
+    this._defaults = defaults;
+    this._name = pluginName;
 
-    console.log(this)
-    console.log(element)
-  }
-}
-
-let rangeSliderObj = new RangeSlider();
-
-document.addEventListener('DOMContentLoaded', () => {
-  createSliderElements("green-range-slider-here");
-});
-
-function createSliderElements(nameContainer) {
-  let containers = document.getElementsByClassName(nameContainer);
-
-  for (let container of containers) {
-    let rangeSlider = document.createElement("div");
-    rangeSlider.className = "green-range-slider grs";
-
-    let rangeSliderButtonMin = document.createElement("div");
-    rangeSliderButtonMin.className = "grs-button-min";
-    let rangeSliderButtonMax = document.createElement("div");
-    rangeSliderButtonMax.className = "grs-button-max";
-    let rangeSliderFilled = document.createElement("div");
-    rangeSliderFilled.className = "grs-filled";
-
-    rangeSlider.append(rangeSliderButtonMin,
-                       rangeSliderFilled,
-                       rangeSliderButtonMax);
-
-    container.append(rangeSlider);
-
-    /**/
-
-    rangeSliderObj.getCoords(rangeSlider);
-  }
-}
-
-/**/
-
-function getCoords(elem) {
-  let coords = elem.getBoundingClientRect();
-  let RangeSlider = {};
-
-  RangeSlider.top = coords.top +
-             window.pageYOffset;
-  RangeSlider.left = coords.left +
-              window.pageXOffset;
-  RangeSlider.rigth = coords.left +
-               window.pageXOffset +
-               coords.width;
-  RangeSlider.bottom = coords.top +
-                window.pageYOffset +
-                coords.height
-  RangeSlider.width = coords.width;
-
-  console.log(RangeSlider)
-  console.log("next")
-}
-
-function moveRange() {
-
-}
-
-function onMouseMove() {
-
-}
-
-function onMouseUp() {
-
-}
+    this.init();
+  };
+  Plugin.prototype.init = function() {
+    //
+  };
+  $.fn[pluginName] = function(options) {
+    return this.each(function() {
+      if (!$.data(this, "plugin_" + pluginName)) {
+        $.data(this, "plugin_" + pluginName,
+        new Plugin(this, options));
+      };
+    });
+  };
+})(jQuery, window, document);
