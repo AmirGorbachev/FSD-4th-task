e64fb0249bcda8bb903f34060a59bb3eef267a52
Amir Gorebachev <32037802+AmirGorbachev@users.noreply.github.com>
2020-10-08 10:39:40 +0600

    Добавление функций вычесления значений
    Добавление функций вычесления значений слайдера и значений слайдера в процентах

--- src/plugin/GrsModel/GrsModel.js
+++ src/plugin/GrsModel/GrsModel.js
@@ -6,9 +6,36 @@ class GrsModel {
     this.options = options;
   }
 
+  calcValue(persentOffset) {
+    let value = ((this.options.maxLimit - this.options.minLimit) *
+                (persentOffset / 100)) + this.options.minLimit;
+
+    let result = Math.round(value / this.options.step) * this.options.step;
+
+    if (result <= this.options.minLimit) {
+      result = this.options.minLimit;
+    } else if (result >= this.options.maxLimit) {
+      result = this.options.maxLimit;
+    };
+
+    return result;
+  }
+
+  calcValuePercentage(persentOffset) {
+    let value = (persentOffset !== undefined) ?
+                this.calcValue(persentOffset) :
+                this.options.minValue;
+
+    let result = (value - this.options.minLimit) /
+                 (this.options.maxLimit - this.options.minLimit) * 100;
+
+    return result;
+  }
+
   updateOptions(option, value) {
     this.options[option] = value;
     console.log(`${option} - ${value}`);
+    console.log(this.options);
   }
 
 }
