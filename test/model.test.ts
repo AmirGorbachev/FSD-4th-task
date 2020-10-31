c4302e5eb658e4200d9b63aed9505c6d2e458982
Amir Gorebachev <32037802+AmirGorbachev@users.noreply.github.com>
2020-11-01 04:31:23 +0700

    Рефакторинг

---
+++ test/model.test.ts
@@ -0,0 +1,8 @@
+import { GrsModel } from '../src/plugin/GrsModel/GrsModel';
+
+describe('GrsModel.getOptions:', () => {
+  test('should been defined', () => {
+    const model = new GrsModel();
+    expect(.getOptions().toBeDefined();
+  });
+});
