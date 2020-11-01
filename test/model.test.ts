93e14bc50db7ae0d8c446fcdb2d259df17c4e0cb
Amir Gorebachev <32037802+AmirGorbachev@users.noreply.github.com>
2020-11-01 07:42:21 +0700

    Первый тест готов

--- test/model.test.ts
+++ test/model.test.ts
@@ -1,8 +1,24 @@
-import { GrsModel } from '../src/plugin/GrsModel/GrsModel';
+import { IOptions} from '../src/plugin/GrsOptions/GrsOptions';
+import { IGrsModel, GrsModel } from '../src/plugin/GrsModel/GrsModel';
 
 describe('GrsModel.getOptions:', () => {
+
   test('should been defined', () => {
-    const model = new GrsModel();
-    expect(.getOptions().toBeDefined();
+
+    const options: IOptions = {
+      minLimit: 0,
+      maxLimit: 100,
+      minValue: 25,
+      maxValue: 75,
+      step: 1,
+      isVertical: false,
+      isInterval: false,
+      withPointers: true,
+      withScale: true,
+    }
+    const model: IGrsModel = new GrsModel(options);
+    expect(model.getOptions()).toBeDefined();
+
   });
+
 });
